dfee98dc9f07a16871535157a1c6655af8f7c750
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-10-02 10:02:45 +1300

    delete all code - re-start from beginning

--- calculator.js
+++ calculator.js
@@ -1,75 +1,11 @@
-// Create a calculator object that contain: 
-//entries array empty to store data, 
-//var valueDisplay set to zero
-
-
-const calculator = {
-    entries : [],
-    valueDisplay: "0",
-    onHoldValue
-}
-
-// create variable that will hold the number entered first
-
-
-
-//Selectors
-const decimal = document.getElementById("decimal");
-const clear = document.getElementById("clear");
-const clearLastEntry = document.getElementById("clearLastEntry");
-const displayValues = document.getElementById("displayValues");
-const btnNumbers = document.getElementsByClassName("numbers");
-const btnOperators = document.getElementById("btn-operator");
-
-
-
-//when I click a button I want the value displayed on the screen
-//eventListener for all the button numbers
-for(let i = 0; i < btnNumbers.length; i++) {
-    btnNumbers[i].addEventListener("click", updateDisplayValue)
-}
-//eventListener for all the button operators
-for(let i = 0; btnOperators.lenght; i++) {
-    btnOperators[i].addEventListener("click", performOperation)
-}
-
-//create a function to update the value display on the screen when a button is clicked
-// when the button is clicked, the input is assign to a variable 
-//check inside the function if the display value is 0 or not. If yes, it has to be set to empty string
-function updateDisplayValue(e) {
-    //Use e.target.innerText to find the content of a button.
-    var btnContent = e.target.innerText;
-    if(valueDisplay === "0") {
-        valueDisplay = "";
-    }
-    //add the content of btnContent to valueDisplay and display it
-    valueDisplay += btnContent;
-    displayValues.innerText = valueDisplay;
-}
-
-//Function that performs the operations
-//check which operator is selected by the user
-//update initial value, onHoldvalue and entries array
-function performOperation(e) {
-    //use e.target.innerText to check which operator is used
-    var operator = e.target.innerText;
-
-    if(operator === "+"){
-        //update the value on hold to the value displayed on the screen
-        onHoldValue = valueDisplay;
-        //display value set to 0 
-        valueDisplay = "0";
-        //display value displayed on the screen
-        displayValues.innerText = valueDisplay;
-        //the "on hold" value is stored into the entries array along with the operator
-        entries.push(onHoldValue);
-        entries.push("+");
-    }
-
-    
-}
-
-
+// Create an entries array empty, var tot set to zero
+// create var temp that is an empty string
+// when the button is clicked, the input is assign to a variable val
+//the value of val is a number or a decimal point, it is added to variable temp and display 
+//to screen (max 10 characters)
+// Got a number, add to temp
+// Got some symbol other than equals, add temp to our entries
+// then add our current symbol and clear temp
 //if user press the AC button all the values stored in the variables are cleared, and the display as well
 // Clear last entry
 //if user press the CE button, the last value is cleared
