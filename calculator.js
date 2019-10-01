b61d299995bb4c4bc4cb014d8964675198263a44
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-10-02 09:29:14 +1300

    change name of initValue to valueDisplay
    add function that perform opearations, starting with addition

--- calculator.js
+++ calculator.js
@@ -1,11 +1,11 @@
 // Create a calculator object that contain: 
 //entries array empty to store data, 
-//var initValue set to zero
+//var valueDisplay set to zero
 
 
 const calculator = {
     entries : [],
-    initValue: "0",
+    valueDisplay: "0",
     onHoldValue
 }
 
@@ -26,32 +26,50 @@ const btnOperators = document.getElementById("btn-operator");
 //when I click a button I want the value displayed on the screen
 //eventListener for all the button numbers
 for(let i = 0; i < btnNumbers.length; i++) {
-    btnNumbers[i].addEventListener("click", //function)
+    btnNumbers[i].addEventListener("click", updateDisplayValue)
 }
 //eventListener for all the button operators
 for(let i = 0; btnOperators.lenght; i++) {
-    btnOperators.addEventListener("click", //function)
+    btnOperators[i].addEventListener("click", performOperation)
 }
 
 //create a function to update the value display on the screen when a button is clicked
 // when the button is clicked, the input is assign to a variable 
-//check inside the function if the display value is 0 or not. If yes, it has to be set to 0
-function displayEnteredValue(e) {
+//check inside the function if the display value is 0 or not. If yes, it has to be set to empty string
+function updateDisplayValue(e) {
+    //Use e.target.innerText to find the content of a button.
     var btnContent = e.target.innerText;
-    if(initValue === "0") {
-        initValue = "";
+    if(valueDisplay === "0") {
+        valueDisplay = "";
     }
+    //add the content of btnContent to valueDisplay and display it
+    valueDisplay += btnContent;
+    displayValues.innerText = valueDisplay;
 }
-//add the content of btnContent to initValue and display it
-    initValue += btnContent;
-    displayValues.innerText = initValue;
+
+//Function that performs the operations
+//check which operator is selected by the user
+//update initial value, onHoldvalue and entries array
+function performOperation(e) {
+    //use e.target.innerText to check which operator is used
+    var operator = e.target.innerText;
+
+    if(operator === "+"){
+        //update the value on hold to the value displayed on the screen
+        onHoldValue = valueDisplay;
+        //display value set to 0 
+        valueDisplay = "0";
+        //display value displayed on the screen
+        displayValues.innerText = valueDisplay;
+        //the "on hold" value is stored into the entries array along with the operator
+        entries.push(onHoldValue);
+        entries.push("+");
+    }
+
+    
+}
 
 
-
-
-// Got a number, add to temp
-// Got some symbol other than equals, add temp to our entries
-// then add our current symbol and clear temp
 //if user press the AC button all the values stored in the variables are cleared, and the display as well
 // Clear last entry
 //if user press the CE button, the last value is cleared
