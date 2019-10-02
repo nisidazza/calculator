32b3330ad64e537c51f6d8e195d880a0c1231780
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-10-02 17:37:52 +1300

    add function to clear screen and insert decimal point
    working on function that clear last entry

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ var onHoldVal;
 
 //create selectors
 var clear = document.getElementById("clear");
+var clearLastEntry = document.getElementById("clearLastEntry");
 var displayValues = document.getElementById("displayValues");
 var decimal = document.getElementById("decimal");
 var btnNumbers = document.getElementsByClassName("btn-number");
@@ -21,7 +22,7 @@ function updateDisplayValue(e) {
     var btnContent = e.target.innerText;
     //Create if statement inside this function that will check if the display value is currently 0 or not. 
     //If yes, set it to an empty string
-    if(currentVal === "0") {
+    if (currentVal === "0") {
         currentVal = "";
     }
     //add the content of btnContent to currentVal
@@ -30,63 +31,92 @@ function updateDisplayValue(e) {
 }
 // create function that performs operations
 //
-    function performOperation(e) {
-        var operator = e.target.innerText; //check content of operator
-        //check witch operator is typed
-        switch (operator) {
-            case "+": 
-                onHoldVal = currentVal;
-                currentVal = "0";
-                displayValues.innerText = currentVal;
-                entriesArr.push(onHoldVal);
-                entriesArr.push("+");
-                break;
-            
-                case "-": 
-                onHoldVal = currentVal;
-                currentVal = "0";
-                displayValues.innerText = currentVal;
-                entriesArr.push(onHoldVal);
-                entriesArr.push("-");
-                break;
+function performOperation(e) {
+    var operator = e.target.innerText; //check content of operator
+    //check witch operator is typed
+    switch (operator) {
+        case "+":
+            onHoldVal = currentVal;
+            currentVal = "0";
+            displayValues.innerText = currentVal;
+            entriesArr.push(onHoldVal);
+            entriesArr.push("+");
+            break;
 
-                case "x": 
-                onHoldVal = currentVal;
-                currentVal = "0";
-                displayValues.innerText = currentVal;
-                entriesArr.push(onHoldVal);
-                entriesArr.push("*");
-                break;
+        case "-":
+            onHoldVal = currentVal;
+            currentVal = "0";
+            displayValues.innerText = currentVal;
+            entriesArr.push(onHoldVal);
+            entriesArr.push("-");
+            break;
 
-                case "÷": 
-                onHoldVal = currentVal;
-                currentVal = "0";
-                displayValues.innerText = currentVal;
-                entriesArr.push(onHoldVal);
-                entriesArr.push("/");
-                break;
+        case "x":
+            onHoldVal = currentVal;
+            currentVal = "0";
+            displayValues.innerText = currentVal;
+            entriesArr.push(onHoldVal);
+            entriesArr.push("*");
+            break;
 
-                case "=": 
-                entriesArr.push(currentVal);
-                //with "join the array becomes a string" and  we pass it to the eval()
-                var evaluateArgument = eval(entriesArr.join(" "));
-                currentVal = evaluateArgument + "";
-                console.log(typeof currentVal);
-                displayValues.innerText = currentVal;
-                entriesArr = []; //clear the array for new input
-                break;
-                default:
-                    break;
-        }
+        case "÷":
+            onHoldVal = currentVal;
+            currentVal = "0";
+            displayValues.innerText = currentVal;
+            entriesArr.push(onHoldVal);
+            entriesArr.push("/");
+            break;
+
+        case "=":
+            entriesArr.push(currentVal);
+            //with "join the array becomes a string" and  we pass it to the eval()
+            var evaluateArgument = eval(entriesArr.join(" "));
+            currentVal = evaluateArgument + "";
+            console.log(typeof currentVal);
+            displayValues.innerText = currentVal;
+            entriesArr = []; //clear the array for new input
+            break;
+        default:
+            break;
     }
+}
 
 // the calculation is performed once the equal button is pressed and the result is displayed on the screen
 //create event listener for buttons:
 // numbers 
-for(var i=0; i<btnNumbers.length; i++) {
+for (var i = 0; i < btnNumbers.length; i++) {
     btnNumbers[i].addEventListener("click", updateDisplayValue);
 }
 //operators
-for(var i=0; i<btnOperators.length; i++) {
+for (var i = 0; i < btnOperators.length; i++) {
     btnOperators[i].addEventListener("click", performOperation);
 }
+
+//create function to clear display
+clear.onclick = () => {
+    currentVal = "0";
+    onHoldVal = undefined;
+    entriesArr = [];
+    displayValues.innerHTML = currentVal;
+}
+
+//create a function to insert decimal point
+decimal.onclick = () => {
+    if(currentVal.includes(".") === false) {
+        currentVal += ".";
+    }
+
+    displayValues.innerText = currentVal;
+}
+
+//create a function that deletes the last entry
+/*clearLastEntry.onclick = () => {
+    currentVal = displayValues.substr(0, currentVal.length - 1);
+    displayValues.innerHTML = currentVal;
+}*/
+
+
+
+
+
+
